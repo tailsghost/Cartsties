9f0a59e24ca8e721a0e04c02117a7ba1e18f0737
AndreyXP <andreyapple99@mail.ru>
2024-03-31 21:03:49 +0300

    Добавление Consumer, outbox

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,3 +1,4 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,18 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddMassTransit(x =>
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
---
+++ src/SearchService/Consumers/AuctionDeletedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionDeletedConsumer
+{
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        Console.WriteLine("--> Consuming AuctionDeleted: " + context.Message.Id);
+
+        var result = await DB.DeleteAsync<Item>(context.Message.Id);
+
+        if (!result.IsAcknowledged) 
+            throw new MessageException(typeof(AuctionDeleted), "Problem deleting auction");
+    }
+}
